5145be7b0075683da466b0868b699df4f09965ae
jtlee <2287781216@qq.com>
2021-11-22 21:10:01 +0800

    deploy

---
+++ precache-manifest.395882471ee8f62fb44796a702c9b34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70868d6eb7220603ec3f",
+    "url": "/bluecar-website/css/app.c3725094.css"
+  },
+  {
+    "revision": "eef2bf0e1db7d2d1859483d95ff8c66b",
+    "url": "/bluecar-website/index.html"
+  },
+  {
+    "revision": "70868d6eb7220603ec3f",
+    "url": "/bluecar-website/js/app.f11380e8.js"
+  },
+  {
+    "revision": "486f07f4e40e37827543",
+    "url": "/bluecar-website/js/chunk-vendors.180b1bde.js"
+  },
+  {
+    "revision": "91daf0028a8d675578e569d93667ad1b",
+    "url": "/bluecar-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bluecar-website/robots.txt"
+  }
+]);
